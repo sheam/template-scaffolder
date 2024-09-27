41c2eba662959341e99608c217bc95f90c84d8bf
Shea Martin <shea@brewninja.net>
2024-09-27 11:37:44 -0300

    move ts-config so it is distrubted

--- scaffolding/typescript-config/scaffolding.config.ts
+++ scaffolding/typescript-config/scaffolding.config.ts
@@ -1,15 +1,15 @@
-import {IConfigFile, Question} from "../../src/types";
+import { IConfigFile, Question } from "../../src/types.js";
 
 export default {
     name: 'typescript',
     description: 'for testing velocity templates',
     srcRoot: 'src',
-    variables: (name: string, input: any): any => ({
+    variables: (name: string, input: object): any => ({
         NEW_VARIABLE: `NEW__${name}`,
     }),
     prompts: (name: string): Question[] => [
         {
-            type: "list",
+            type: "select",
             name: "COLOR",
             message: "Enter your favourite color: ",
             choices: [
